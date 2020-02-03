169cadf444edf5a2a4b03aa2364cd4efe66d5387
2dcon <2jicon@gmail.com>
2020-02-04 02:22:59 +0800

    Update upgradeCentos7Kernel.sh

--- upgradeCentos7Kernel.sh
+++ upgradeCentos7Kernel.sh
@@ -2,6 +2,6 @@ yum -y update
 yum -y install yum-plugin-fastestmirror
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-yum --enablerepo=elrepo-kernel install kernel-ml
+yum -y --enablerepo=elrepo-kernel install kernel-ml
 grub2-set-default 0
 reboot
