1763ac52886b77141cc7829894e01e61559a863c
2dcon <2jicon@gmail.com>
2020-02-06 22:34:41 +0800

    Create wordpress.sh

---
+++ wordpress.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Install Apache
+yum -y install httpd
+systemctl start httpd.service
+systemctl enable httpd.service
+
+#Install MySQL (MariaDB)
+yum -y install mariadb-server mariadb
+systemctl start mariadb
+mysql_secure_installation
+
+systemctl enable mariadb.service
+
+#Install PHP 7.4
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum -y install yum-utils
+yum-config-manager --enable remi-php74
