0053c65ae3ad8116913a8a762b7a94e14434b8b6
2dcon <2jicon@gmail.com>
2020-02-25 21:14:37 +0800

    Update random_ssh_port.sh

--- random_ssh_port.sh
+++ random_ssh_port.sh
@@ -2,4 +2,6 @@
 
 PORT=$((1000 + RANDOM % 10000))
 echo "Port $PORT" >> /etc/ssh/sshd_config
+firewall-cmd --permanent --add-port=$PORT/tcp
+sudo firewall-cmd --reload
 echo "The new ssh port is: $PORT"
