d30c7ed57ac0c39404aabc44e6f8a55854c66e87
2dcon <2jicon@gmail.com>
2020-03-01 22:48:45 +0800

    Update random_ssh_port.sh

--- random_ssh_port.sh
+++ random_ssh_port.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+FILE="/etc/ssh/sshd_config"
+
+sed -i '/^[Port 22]/d' $FILE
+
 PORT=$((1000 + RANDOM % 9000))
-echo "Port $PORT" >> /etc/ssh/sshd_config
+echo "Port $PORT" >> $FILE
 firewall-cmd --permanent --add-port=$PORT/tcp
 sudo firewall-cmd --reload
+
+#Add public ssh key
+mkdir ~/.ssh/
+
+echo 'Paste your public key'
+read KEY
+echo $KEY >> ~/.ssh/authorized_keys
+
+#Disable password login
+sed -i '/^[PasswordAuthentication]/d' $FILE
+echo "PasswordAuthentication no" >> $FILE
+
 echo "The new ssh port is: $PORT"
