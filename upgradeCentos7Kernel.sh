4f14800c952023b8c84e6d9b6d448b3c69e25e94
2dcon <2jicon@gmail.com>
2020-05-26 09:55:34 +0800

    Update upgradeCentos7Kernel.sh

--- upgradeCentos7Kernel.sh
+++ upgradeCentos7Kernel.sh
@@ -16,10 +16,12 @@ fi
 
 #for CentOS 7
 if [[ $VER == 7 ]]; then
+	yum -y update
+	
 	rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 	rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-	yum -y update
 	yum -y --enablerepo=elrepo-kernel install kernel-ml
+	
 	grub2-set-default 0
 #for CentOS 8
 else
